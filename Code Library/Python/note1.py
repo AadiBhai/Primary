6b5ac3cb2db8ccdd4cc334daa4dc5d647416879e
Aaditya Assalkar <aadityaassalkar@gmail.com>
2022-12-02 20:16:54 +0530

    new python course started

---
+++ Code Library/Python/note1.py
@@ -0,0 +1,5 @@
+#escape sequence character and comments
+# This is comment
+print("see you in the next line\n\"Jai Shree Ram\"") # Here \n and \" are escape sequence character
+print(1, 3, 4+5, sep="    ", end="\n   ")
+print("jadu")
