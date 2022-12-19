3059f8b43df57e7ccd616ceb381eca58b0a2415b
Aaditya Assalkar <aadityaassalkar@gmail.com>
2022-12-19 12:56:38 +0530

    py note6 and js note15 added

--- Code Library/Python/note6.py
+++ Code Library/Python/note6.py
@@ -12,3 +12,45 @@ mean2(x, y) # this is how we call the function
 # if we don't want to define the function but only declare it then we use pass keyword as follow
 def complex(a, b):
     pass # by doing this we can define the function anytime and anywhere in the code
+
+# Function arguments
+# There are 4 types of arguments
+# 1. Default arguments
+def fun1(c = 4, d = 6): # here we provided 4 and 6 as the default values of c and d hence these are default arguments
+    return c + d # return statement is used to return the value of the expression back to the calling function
+    return 5 # if we provide multiple return statements it only consider first one always
+
+z = fun1() # here fun1 will consider the default values and execute
+z = fun1(6) # here 6 is the value of c and default value of d is considered here to do the same for d we do as follows
+z = fun1(d = 10)
+print(z)
+
+# 2. keyword arguments
+# we provide the arguments with key = value while calling the function this way interpreter recognize the arguments by parameter name
+z = fun1(d = 15, c = 15)
+print(z)
+
+# 3. required arguments
+# if we are using n number of parameters in the function defination then it is necessary to pass n number of arguments to the function otherwise it will give error
+
+# def sum3(c, d, e):
+#     return c + d + e
+
+# z = sum3(5, 9) # here number of arguments passed does not match the number of actual number of aurguments hence it will give error
+# print(z)
+
+# 4. variable-length arguments
+# i) Arbitary arguments
+def avarage(*numbers): # using * before parameter name will consider the parameter as tuple
+    sum = 0
+    for i in numbers:
+        sum = sum + i
+    print("Avarage of all numbers is ", sum/len(numbers))
+
+avarage(5, 4, 3, 2, 1) # now we can pass any number of arguments
+
+# ii) Keyword arbitary arguments
+def myName(**name): # using ** before parameter name will consider the parameter as dictionary
+    print("Hello, I am ", name["fname"], name["mname"], name["lname"]) # paraname["argument-name"] this is the syntax to define arguments
+
+myName(fname = "Aaditya", mname = "Ganeshrao", lname = "Assalkar") # this is how we pass the arguments
---
+++ Code Library/Web Devlopment/CoffeeScript/CoffeeScript_note15/Script.js
@@ -0,0 +1,12 @@
+let first = document.getElementById("first")
+let a = first.getAttribute("class") // used to get the value of an attribute
+console.log(a)
+console.log(first.hasAttribute("class")) // used to check the existance of attribute
+console.log(first.hasAttribute("hidden")) // used to check the existance of attribute
+console.log(first.setAttribute("hidden", "true")) // used to set a new attribute
+console.log(first.setAttribute("class", "true just")) // if set the existing attribute it removes the existing value and add the new value
+console.log(first.removeAttribute("hidden")) // used to remove attribute
+console.log(first.attributes) // used to list out all the attribute
+console.log(first.dataset) // used to list out all custom attributes created usin 'data-'
+console.log(first.dataset.game)
+console.log(first.dataset.player)
